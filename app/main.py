1f99d1feaaed0318cc7a8531d74bbcaa1641a202
TrabsonAlves <rafaelalves156.br@gmail.com>
2025-08-17 15:49:39 -0300

    Teste modif para CI CD

--- app/main.py
+++ app/main.py
@@ -99,3 +99,7 @@ def read_clientes(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)
 @app.get("/")
 def read_root():
   return {"Hello": "World"}
+
+@app.get("/teste")
+def read_root():
+  return {"teste": "teste"}
